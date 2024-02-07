59febc803707b9d395b91e6cf1f96d52a35ddbca
Pankaj Jaiswal <jaiswal.pankaj10@gmail.com>
2024-02-07 11:04:03 +0530

    Add files via upload

---
+++ ms-kubernetes/kubernetes-with-cloud-discovery/accounts/src/main/java/com/clover/accounts/dto/AccountsDto.java
@@ -0,0 +1,33 @@
+package com.clover.accounts.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+@Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
+public class AccountsDto {
+
+    @NotEmpty(message = "AccountNumber can not be a null or empty")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+            description = "Account Number of Clover account", example = "3454433243"
+    )
+    private Long accountNumber;
+
+    @NotEmpty(message = "AccountType can not be a null or empty")
+    @Schema(
+            description = "Account type of Clover account", example = "Savings"
+    )
+    private String accountType;
+
+    @NotEmpty(message = "BranchAddress can not be a null or empty")
+    @Schema(
+            description = "Clover branch address", example = "123 NewYork"
+    )
+    private String branchAddress;
+}
---
+++ ms-kubernetes/kubernetes-with-cloud-discovery/accounts/src/main/java/com/clover/accounts/service/client/LoansFallback.java
@@ -0,0 +1,14 @@
+package com.clover.accounts.service.client;
+
+import com.clover.accounts.dto.LoansDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoansFallback implements LoansFeignClient{
+
+    @Override
+    public ResponseEntity<LoansDto> fetchLoanDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
